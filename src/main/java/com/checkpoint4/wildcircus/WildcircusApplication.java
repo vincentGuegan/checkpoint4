43d788b61de8af263f0aefa35d6dad545bd3b897
Vincent Guegan <gueganvincent@gmail.com>
2019-10-09 11:03:42 +0200

    first commit

---
+++ src/main/java/com/checkpoint4/wildcircus/WildcircusApplication.java
@@ -0,0 +1,13 @@
+package com.checkpoint4.wildcircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcircusApplication.class, args);
+	}
+
+}
